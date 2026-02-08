cc2fe26aa1a49429a6d7d08cb989b838f5fd3789
Guei Amos <gueiamos1401@gmail.com>
2026-02-08 19:35:44 +0000

    lancement api tarif au demarrage

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchBackofficeConfig } from '../../redux/slices/backofficeSlice';
+import { fetchTarifs, fetchGroupedTarifs } from '../../redux/slices/tarificationSlice';
+import { fetchZones } from '../../redux/slices/zoneSlice';
+import { fetchAgences } from '../../redux/slices/agenceSlice';
+import { fetchProduits } from '../../redux/slices/produitSlice';
 import SettingsModal from './SettingsModal';
 import Sidebar from './Sidebar';
 import Topbar from './Topbar';
@@ -10,12 +14,27 @@ const Layout = ({ children }) => {
   const [showConfigModal, setShowConfigModal] = useState(true);
   const dispatch = useDispatch();
   const { isConfigured, loading } = useSelector((state) => state.backoffice);
+  const { hasLoaded: tarifsLoaded } = useSelector((state) => state.tarification);
+  const { hasLoaded: zonesLoaded } = useSelector((state) => state.zones);
+  const { hasLoaded: agencesLoaded } = useSelector((state) => state.agences);
+  const { hasLoadedProduits: produitsLoaded } = useSelector((state) => state.produits);
 
   useEffect(() => {
     if (loading === 'idle') {
       dispatch(fetchBackofficeConfig());
     }
   }, [dispatch, loading]);
+
+  // Chargement des données globales au démarrage
+  useEffect(() => {
+    if (!tarifsLoaded) {
+      dispatch(fetchTarifs());
+      dispatch(fetchGroupedTarifs());
+    }
+    if (!zonesLoaded) dispatch(fetchZones());
+    if (!agencesLoaded) dispatch(fetchAgences());
+    if (!produitsLoaded) dispatch(fetchProduits());
+  }, [dispatch, tarifsLoaded, zonesLoaded, agencesLoaded, produitsLoaded]);
 
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
